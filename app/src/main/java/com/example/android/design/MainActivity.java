de49783c8569d371c8ff52147eb194f14534cafd
Sunday David <sundaydavid989@gmail.com>
2019-09-19 13:42:32 +0100

    simple design

---
+++ app/src/main/java/com/example/android/design/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.design;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signUp(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+}
